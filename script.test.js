5bb1208118731a87b52a783860419b6057edc7d1
Jackal_lzt <bkyarokun@gmail.com>
2023-12-04 10:07:17 +0800

    chore(6): use .toEqual to compare object
    	since .toBe is strict equality and can not compare two objects, we use .toEqual instead, this method do not consider different memory address.

--- script.test.js
+++ script.test.js
@@ -3,6 +3,14 @@ test("adds 2 + 3 to equal 5", () => {
   expect(script.addNum(2, 3)).toBe(5);
 });
 
+// error, .toBe is strict equality, cloned obj has different memory address, so it is not equal
 test("clone an object", () => {
   expect(script.cloneObj({ a: 1, b: 2 })).toBe({ a: 1, b: 2 });
+});
+
+// passed, toEqual can be used to compare objects(only compare key&value, do not consider memory address)
+test("compare object with .toEqual", () => {
+  var obj1 = { a: 1 };
+  var obj2 = { a: 1 };
+  expect(obj1).toEqual(obj2);
 });
